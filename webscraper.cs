40620287bc5dd68daa3851a67faa3a74ae8819ef
NovaReturn <40554463+NovaReturn@users.noreply.github.com>
2023-02-26 22:44:30 -0600

    Update Webscraper
    Updated the file at home. Works as expected. Changed conditions, WriteLine, and location information.

--- webscraper.cs
+++ webscraper.cs
@@ -4,38 +4,39 @@ using HtmlAgilityPack;
 using System;
 using System.Net.Http;
 
-namespace
+namespace Webscraper
 {
-	class Program
-	{
-		static void Main(String[] args)
-		{
-			// using weather.com as the example, with current location of montgomery, AL
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            // using weather.com as the example, with current location of montgomery, AL
 
-			//send get request to weather.com
-			String URL "https://weather.com/weather/today/l/229d98c080fe2c298000be41854858e60aba6bd0a867b53a8666de41ff918fbe";
-			var httpClient = new HttpClient();
+            //send get request to weather.com
+            String url = "https://weather.com/weather/today/l/229d98c080fe2c298000be41854858e60aba6bd0a867b53a8666de41ff918fbe";
+            var httpClient = new HttpClient();
 
-			var html = httpClient.GetStringAsync(url).Result;
-			var htmlDocument = new HtmlDocument();
+            var html = httpClient.GetStringAsync(url).Result;
+            var htmlDocument = new HtmlDocument();
 
-			htmlDocument.LoadHtml(html);
+            htmlDocument.LoadHtml(html);
 
 
-			// get the temperature
-			 var temperatureElement = htmlDocument.DocumentNode.SelectSingleNode("//span[@class='CurrentConditions--tempvalue--MHmYY']");
-			 var tempurature = temperatureElement.InnerText.Trim();
-			 Console.writeline("Temperature: " + tempurature);
-			 // getg the conditions
-			 var conditionElement = htmlDocument.DocumentNode.SelectSingleNode("//div[@class='CurrentConditions--phraseValue--mZC']");
-			 var tempurature = conditionElement.InnerText.Trim();
-			 Console.writeline("Conditions: " + conditions);
+            // get the temperature
+            var temperatureElement = htmlDocument.DocumentNode.SelectSingleNode("//span[@class='TodayDetailsCard--feelsLikeTempValue--2icPt']");
+            var tempurature = temperatureElement.InnerText.Trim();
+            Console.WriteLine("Temperature: " + tempurature);
 
-			//get location
-			 var locationElement = htmlDocument.DocumentNode.SelectSingleNode("//div[@class='CurrentConditions--header--kbXKR']");
-			 var location = locationElement.InnerText.Trim();
-			 Console.writeline("Location: " + location);
-		}
-	}
+            // get the conditions
+            var conditionElement = htmlDocument.DocumentNode.SelectSingleNode("//div[@class='CurrentConditions--phraseValue--mZC_p']");
+            var conditions = conditionElement.InnerText.Trim();
+            Console.WriteLine("Conditions: " + conditions);
+
+            //get location
+            var locationElement = htmlDocument.DocumentNode.SelectSingleNode("//h1[@class='CurrentConditions--location--1YWj_']");
+            var location = locationElement.InnerText.Trim();
+            Console.WriteLine("Location: " + location);
+        }
+    }
 
 }
